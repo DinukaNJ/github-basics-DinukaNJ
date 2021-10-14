5d3c274e76e772405e40a6d7649a8e9ccfcc7f6b
Dinuka_NJ <dinuka@madhurasri.com>
2021-10-14 23:18:31 +0530

    g

--- frontend/src/components/AddTutorial.vue
+++ frontend/src/components/AddTutorial.vue
@@ -82,7 +82,7 @@ export default {
     // Create New product
     async AddTutorial() {
       try {
-        await axios.post("https://tutorial-management.herokuapp.com/", {
+        await axios.post("https://tutorial-management.herokuapp.com/tutorials", {
           title: this.title,
           description: this.description,
         });
