9cfb9817dcbebdd58f362900d7574cffe2eab6d1
Dinuka_NJ <dinuka@madhurasri.com>
2021-10-14 22:29:35 +0530

    cinfig

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2";
    
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: 'us-cdbr-east-04.cleardb.com',
   user: 'b9df1c33fabef7',
   password: '799e9eb7',
   database: 'heroku_f84aba608bed187'
